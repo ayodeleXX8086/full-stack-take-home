fbdfb2884b0ec7f66b8d6e942b6c5c04bfeaa26d
Ayomide Ayodele <ayomidea@Ayomides-MacBook-Pro.local>
2023-07-20 15:28:58 -0400

    API fix to include natureCodeId

--- api/app/graphql/mutations/update_chatroom.rb
+++ api/app/graphql/mutations/update_chatroom.rb
@@ -6,22 +6,32 @@ module Mutations
     argument :caller_phone_number, String, required: false
     argument :description, String, required: false
     argument :resolved, Boolean, required: false
+    argument :nature_code_id, ID, required: false
 
     # Field to return the updated chatroom
     field :chatroom, Types::ChatroomType, null: false
 
     # Resolver
-    def resolve(id:, label: nil, caller_phone_number: nil, description: nil, resolved: nil)
-      p "Updating chat room #{id}, #{caller_phone_number}, #{description}, #{resolved}"
+    def resolve(id:, label: nil, caller_phone_number: nil, description: nil, resolved: nil, nature_code_id: nil)
       # Find the chatroom by the provided id or throw an exception if the chatroom is not found.
       chatroom = Chatroom.find(id)
 
+      # Update the chatroom attributes with the provided values or use existing values if nil.
+      label ||= chatroom.label
+      caller_phone_number ||= chatroom.caller_phone_number
+      description ||= chatroom.description
+      resolved = chatroom.resolved if resolved.nil?
+      nature_code_id ||= chatroom.nature_code_id
+
+      p "Nature code #{nature_code_id}"
+
       # Update the chatroom attributes with the provided values
       result = chatroom.update(
-        label: label,
-        caller_phone_number: caller_phone_number,
-        description: description,
-        resolved: resolved
+        label:,
+        caller_phone_number:,
+        description:,
+        resolved:,
+        nature_code_id:
       )
 
       if result
--- api/spec/graphql/mutations/update_chatroom_spec.rb
+++ api/spec/graphql/mutations/update_chatroom_spec.rb
@@ -6,15 +6,6 @@ RSpec.describe "Mutations::UpdateChatroom", type: :request do
   let(:description) { "Updated description" }
   let(:caller_phone_number) { "1234567890" }
   let(:nature_code_id) { nature_code.id }
-  
-  let(:variables) do
-    {
-      id: nil,
-      label: label,
-      callerPhoneNumber: caller_phone_number,
-      description: description,
-    }
-  end
 
   let(:query) do
     <<~GQL
@@ -24,21 +15,28 @@ RSpec.describe "Mutations::UpdateChatroom", type: :request do
         $callerPhoneNumber: String
         $description: String
         $resolved: Boolean
+        $natureCodeId: ID
       ) {
-        updateChatroom(
-          input: {
-            id: $id
-            label: $label
-            callerPhoneNumber: $callerPhoneNumber
-            description: $description
-            resolved: $resolved
-          }
+         updateChatroom(
+            input: {
+                id: $id
+                label: $label
+                callerPhoneNumber: $callerPhoneNumber
+                description: $description
+                resolved: $resolved
+                natureCodeId: $natureCodeId
+            }
         ) {
           chatroom {
             id
             label
+            callerPhoneNumber
             description
             resolved
+            natureCode {
+              id
+              name
+            }
           }
         }
       }
@@ -46,19 +44,48 @@ RSpec.describe "Mutations::UpdateChatroom", type: :request do
   end
 
   context "when updating a chat room that exists" do
-    it "updates the chat room" do
+    before do
+      @variables = {} # Define @variables as an instance variable
+    end
+    it "update the chat room description" do
       # Create a chatroom to be updated
       chatroom = Chatroom.create(
-        label: "Old Label",
-        caller_phone_number: "+9876543210",
+        label:,
+        caller_phone_number:,
         description: "Old description",
         nature_code_id: nature_code.id,
         resolved: false
       )
-      variables[:id] = chatroom.id
-      variables[:resolved]=false
+      @variables[:id] = chatroom.id
+      @variables[:description]=description
 
-      expect { post '/graphql', params: { query: query, variables: variables.to_json } }.to_not change { Chatroom.count }
+      expect { post '/graphql', params: { query: query, variables: @variables.to_json } }.to_not change { Chatroom.count }
+
+      response_json = JSON.parse(response.body)
+      p "Response json #{response_json}"
+      chatroom = Chatroom.find(response_json['data']['updateChatroom']['chatroom']['id'])
+
+      expect(chatroom).to be_truthy
+      expect(chatroom.label).to eq(label)
+      expect(chatroom.description).to eq(description)
+      expect(chatroom.caller_phone_number).to eq(caller_phone_number)
+      expect(chatroom.nature_code_id).to eq(nature_code_id)
+    end
+
+    it "update the chat room with resolved" do
+      # Create a chatroom to be updated
+      chatroom = Chatroom.create(
+        label:,
+        caller_phone_number:,
+        description: "Old description",
+        nature_code_id: nature_code.id,
+        resolved: false
+      )
+      @variables[:id] = chatroom.id
+      @variables[:description]=description
+      @variables[:resolved] = true
+
+      expect { post '/graphql', params: { query: query, variables: @variables.to_json } }.to_not change { Chatroom.count }
 
       response_json = JSON.parse(response.body)
       chatroom = Chatroom.find(response_json['data']['updateChatroom']['chatroom']['id'])
@@ -68,17 +95,19 @@ RSpec.describe "Mutations::UpdateChatroom", type: :request do
       expect(chatroom.description).to eq(description)
       expect(chatroom.caller_phone_number).to eq(caller_phone_number)
       expect(chatroom.nature_code_id).to eq(nature_code_id)
+      expect(chatroom.resolved).to eq(true)
     end
   end
+
 
   context "when chat room does not exist" do
     let(:non_existing_id) { "non_existing_id" }
     before do
-      variables[:id] = non_existing_id
+      @variables = {id: non_existing_id}
     end
 
     it "returns an error" do
-      post '/graphql', params: { query: query, variables: variables.to_json }
+      post '/graphql', params: { query: query, variables: @variables.to_json }
 
       response_json = JSON.parse(response.body)
 
